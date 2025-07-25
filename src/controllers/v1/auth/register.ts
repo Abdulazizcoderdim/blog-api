207765f10ced0f765959c8fe2e38cd52ace05bd7
AbdulazizRus <coderdim@gmail.com>
2025-07-25 21:18:38 +0500

    Add user model with password hashing and username generation utility

--- src/controllers/v1/auth/register.ts
+++ src/controllers/v1/auth/register.ts
@@ -1,10 +1,30 @@
 import { logger } from "@/lib/winston";
+import type { IUser } from "@/models/user";
+import User from "@/models/user";
+import { genUsername } from "@/utils";
 import type { Request, Response } from "express";
 
+type UserData = Pick<IUser, "email" | "password" | "role">;
+
 const register = async (req: Request, res: Response): Promise<void> => {
+  const { email, password, role } = req.body as UserData;
+
   try {
+    const username = genUsername();
+
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+      role,
+    });
+
     res.status(201).json({
-      message: "New user created",
+      user: {
+        username: newUser.username,
+        email: newUser.email,
+        role: newUser.role,
+      },
     });
   } catch (error) {
     res.status(500).json({
---
+++ src/models/user.ts
@@ -0,0 +1,101 @@
+import bcrypt from "bcrypt";
+import { model, Schema } from "mongoose";
+
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  role: "admin" | "user";
+  firstName?: string;
+  lastName?: string;
+  socialLinks?: {
+    website?: string;
+    facebook?: string;
+    instagram?: string;
+    linkedin?: string;
+    x?: string;
+    youtube?: string;
+  };
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: [true, "Foydalanuvchi nomi talab qilinadi"],
+      maxlength: [20, "Foydalanuvchi nomi 20 ta belgidan oshmasligi kerak"],
+      unique: [true, "Foydalanuvchi nomi noyob bo'lishi kerak"],
+    },
+    email: {
+      type: String,
+      required: [true, "Elektron pochta talab qilinadi"],
+      maxlength: [50, "Elektron pochta 50 belgidan kam bo'lishi kerak"],
+      unique: [true, "Elektron pochta noyob bo'lishi kerak"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password talab qilinadi"],
+      select: false,
+    },
+    role: {
+      type: String,
+      required: [true, "Role talab qilinadi"],
+      enum: {
+        values: ["admin", "user"],
+        message: "{VALUE} qo'llab-quvvatlanmaydi",
+      },
+      default: "user",
+    },
+    firstName: {
+      type: String,
+      maxlength: [20, "First name must be less than 20 characters"],
+    },
+    lastName: {
+      type: String,
+      maxlength: [20, "Last name must be lass than 20 characters"],
+    },
+    socialLinks: {
+      website: {
+        type: String,
+        maxlength: [100, "Website address must be less than 100 characters"],
+      },
+      facebook: {
+        type: String,
+        maxlength: [100, "Facebook address must be less than 100 characters"],
+      },
+      instagram: {
+        type: String,
+        maxlength: [100, "Instagram address must be less than 100 characters"],
+      },
+      linkedin: {
+        type: String,
+        maxlength: [100, "Linkedin address must be less than 100 characters"],
+      },
+      x: {
+        type: String,
+        maxlength: [100, "X address must be less than 100 characters"],
+      },
+      youtube: {
+        type: String,
+        maxlength: [100, "YouTube address must be less than 100 characters"],
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.hashPassword = async function (
+  password: string
+): Promise<string> {
+  return await bcrypt.hash(password, 10);
+};
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default model<IUser>("User", userSchema);
