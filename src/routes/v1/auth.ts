6c71bd1aa30a52ffee7a5305dde0d9e4a86a3175
AbdulazizRus <coderdim@gmail.com>
2025-07-26 14:30:27 +0500

    Add express-validator for registration validation and implement validation error handling

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,8 +1,40 @@
 import register from "@/controllers/v1/auth/register";
+import validationError from "@/middleware/validationError";
+import user from "@/models/user";
 import { Router } from "express";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/register", register);
+router.post(
+  "/register",
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Elektron pochta kerak")
+    .isLength({ max: 50 })
+    .withMessage("Elektron pochta 50 belgidan kam bo'lishi kerak")
+    .isEmail()
+    .withMessage("Yaroqsiz elektron pochta manzillari")
+    .custom(async (value) => {
+      const userExists = await user.exists({ email: value });
+      if (userExists) throw new Error("User already exists.");
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("Password talab qilinadi")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  body("role")
+    .optional()
+    .isString()
+    .withMessage("Role must be a string")
+    .isIn(["admin", "user"])
+    .withMessage("Role must be either admin or user"),
+  validationError,
+  register
+);
+
+router.post("/login");
 
 export default router;
