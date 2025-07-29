af1fe731b7a8d7c6c2a4a2562c81788617be7cf9
AbdulazizRus <coderdim@gmail.com>
2025-07-29 17:01:03 +0500

    Implement updateCurrentUser controller with user data handling and error management

--- src/controllers/v1/user/update_current_user.ts
+++ src/controllers/v1/user/update_current_user.ts
@@ -1,8 +1,48 @@
+import { logger } from "@/lib/winston";
+import User from "@/models/user";
 import type { Request, Response } from "express";
 
 const updateCurrentUser = async (
   req: Request,
   res: Response
-): Promise<void> => {};
+): Promise<void> => {
+  const userId = req.userId;
+
+  const {
+    username,
+    email,
+    password,
+    first_name,
+    last_name,
+    website,
+    facebook,
+    instagram,
+    linkedin,
+    x,
+    youtube,
+  } = req.body;
+
+  try {
+    const user = await User.findById(userId).select("+password -__v").exec();
+
+    if (!user) {
+      res.status(404).json({
+        code: "NotFound",
+        message: "User not found",
+      });
+      return;
+    }
+
+    if (username) user.username = username;
+  } catch (error) {
+    res.status(500).json({
+      code: "ServerError",
+      message: "Internal server error",
+      error,
+    });
+
+    logger.error("Error while updating current user", error);
+  }
+};
 
 export default updateCurrentUser;
