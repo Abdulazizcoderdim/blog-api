35c7694f0edcb2200f21b7faa863d6f847797f99
AbdulazizRus <coderdim@gmail.com>
2025-07-29 13:52:31 +0500

    Implement getCurrentUser controller and update user routes for current user retrieval

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,9 +1,15 @@
+import getCurrentUser from "@/controllers/v1/user/get_current_user";
 import authorize from "@/lib/authorize";
 import authenticate from "@/middleware/authenticate";
 import { Router } from "express";
 
 const router = Router();
 
-router.get("/current", authenticate, authorize(["admin", "user"]));
+router.get(
+  "/current",
+  authenticate,
+  authorize(["admin", "user"]),
+  getCurrentUser
+);
 
 export default router;
