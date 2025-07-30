9f8f0e13107ad9ed0ea69218af89d9c97d19795a
AbdulazizRus <coderdim@gmail.com>
2025-07-30 09:27:31 +0500

    Add validation for username in updateCurrentUser route

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -2,7 +2,10 @@ import getCurrentUser from "@/controllers/v1/user/get_current_user";
 import updateCurrentUser from "@/controllers/v1/user/update_current_user";
 import authorize from "@/lib/authorize";
 import authenticate from "@/middleware/authenticate";
+import validationError from "@/middleware/validationError";
+import User from "@/models/user";
 import { Router } from "express";
+import { body } from "express-validator";
 
 const router = Router();
 
@@ -17,6 +20,18 @@ router.put(
   "/current",
   authenticate,
   authorize(["admin", "user"]),
+  body("username")
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .custom(async (value) => {
+      const userExists = await User.exists({ username: value });
+
+      if (userExists) {
+        throw Error("This username is already in use");
+      }
+    }),
+  validationError,
   updateCurrentUser
 );
 
