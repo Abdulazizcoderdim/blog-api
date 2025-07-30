265a3d5ed16e56be8b48e2c67323f9c61d82a8b7
AbdulazizRus <coderdim@gmail.com>
2025-07-30 09:21:59 +0500

    Fix updateCurrentUser controller to correctly handle user data updates and improve logging

--- src/controllers/v1/user/update_current_user.ts
+++ src/controllers/v1/user/update_current_user.ts
@@ -34,6 +34,24 @@ const updateCurrentUser = async (
     }
 
     if (username) user.username = username;
+    if (email) user.email = email;
+    if (password) user.password = password;
+    if (first_name) user.firstName = first_name;
+    if (last_name) user.lastName = last_name;
+    if (!user.socialLinks) user.socialLinks = {};
+    if (website) user.socialLinks.website = website;
+    if (facebook) user.socialLinks.facebook = facebook;
+    if (instagram) user.socialLinks.instagram = instagram;
+    if (linkedin) user.socialLinks.linkedin = linkedin;
+    if (x) user.socialLinks.x = x;
+    if (youtube) user.socialLinks.youtube = youtube;
+
+    await user.save();
+    logger.info("User update successfullt", user);
+
+    res.status(200).json({
+      user,
+    });
   } catch (error) {
     res.status(500).json({
       code: "ServerError",
