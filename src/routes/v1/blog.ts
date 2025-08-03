592ac0ebd86eb924aed3531f95ade53f43623d67
AbdulazizRus <coderdim@gmail.com>
2025-08-03 12:59:10 +0500

    Add validation for blog creation fields with error handling

--- src/routes/v1/blog.ts
+++ src/routes/v1/blog.ts
@@ -2,7 +2,9 @@ import createBlog from "@/controllers/v1/blog/create_blog";
 import authorize from "@/lib/authorize";
 import authenticate from "@/middleware/authenticate";
 import uploadBlogBanner from "@/middleware/uploadBlogBanner";
+import validationError from "@/middleware/validationError";
 import { Router } from "express";
+import { body } from "express-validator";
 import multer from "multer";
 
 const upload = multer();
@@ -15,6 +17,18 @@ router.post(
   authorize(["admin"]),
   upload.single("banner_image"),
   uploadBlogBanner("post"),
+  body("title")
+    .trim()
+    .notEmpty()
+    .withMessage("Title is required")
+    .isLength({ max: 180 })
+    .withMessage("Title must be less than 180 characters"),
+  body("content").trim().notEmpty().withMessage("Connent is required"),
+  body("status")
+    .optional()
+    .isIn(["draft", "published"])
+    .withMessage("Status must be one of the value, draft or published"),
+  validationError,
   createBlog
 );
 
