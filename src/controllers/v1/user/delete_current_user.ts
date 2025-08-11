e5713a5e0b1c41131149095ee7d96afb7e6ce422
AbdulazizRus <coderdim@gmail.com>
2025-08-11 11:47:50 +0500

    changes

--- src/controllers/v1/user/delete_current_user.ts
+++ src/controllers/v1/user/delete_current_user.ts
@@ -1,5 +1,7 @@
 import { logger } from "@/lib/winston";
+import Blog from "@/models/blog";
 import User from "@/models/user";
+import { v2 as cloudinary } from "cloudinary";
 import type { Request, Response } from "express";
 
 const deleteCurrentUser = async (
@@ -8,8 +10,24 @@ const deleteCurrentUser = async (
 ): Promise<void> => {
   const userId = req.userId;
   try {
-    await User.deleteOne({ _id: userId });
+    const blogs = await Blog.find({ author: userId })
+      .select("banner.publicId")
+      .lean()
+      .exec();
+    const publicIds = blogs.map(({ banner }) => banner.publicId);
+    await cloudinary.api.delete_resources(publicIds);
 
+    logger.info("Multiple blog banners deleted from Coudinary", {
+      publicIds,
+    });
+
+    await Blog.deleteMany({ author: userId });
+    logger.info("Multiple blogs deleted", {
+      userId,
+      blogs,
+    });
+
+    await User.deleteOne({ _id: userId });
     logger.info("A user account has been deleted", {
       userId,
     });
--- src/controllers/v1/user/delete_user.ts
+++ src/controllers/v1/user/delete_user.ts
@@ -1,10 +1,29 @@
 import { logger } from "@/lib/winston";
+import Blog from "@/models/blog";
 import User from "@/models/user";
+import { v2 as cloudinary } from "cloudinary";
 import type { Request, Response } from "express";
 
 const deleteUser = async (req: Request, res: Response) => {
   const userId = req.params.userId;
   try {
+    const blogs = await Blog.find({ author: userId })
+      .select("banner.publicId")
+      .lean()
+      .exec();
+    const publicIds = blogs.map(({ banner }) => banner.publicId);
+    await cloudinary.api.delete_resources(publicIds);
+
+    logger.info("Multiple blog banners deleted from Coudinary", {
+      publicIds,
+    });
+
+    await Blog.deleteMany({ author: userId });
+    logger.info("Multiple blogs deleted", {
+      userId,
+      blogs,
+    });
+
     await User.deleteOne({ _id: userId });
     logger.info("A user account has been deleted", { userId });
 
